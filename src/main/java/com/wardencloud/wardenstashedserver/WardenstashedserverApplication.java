cdeb109fb27f10f20b7130d1fcf2c0c3a3137c7e
de2008de <mrycchou@gmail.com>
2019-09-16 21:59:14 -0700

    first commit

---
+++ src/main/java/com/wardencloud/wardenstashedserver/WardenstashedserverApplication.java
@@ -0,0 +1,13 @@
+package com.wardencloud.wardenstashedserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardenstashedserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardenstashedserverApplication.class, args);
+	}
+
+}
