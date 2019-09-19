1bb574d178e11c4a448497cd2e736571c3ca6bdc
de2008de <mrycchou@gmail.com>
2019-09-18 21:42:00 -0700

    Use fastjson to store returned data from controller

--- src/main/java/com/wardencloud/wardenstashedserver/controllers/CardController.java
+++ src/main/java/com/wardencloud/wardenstashedserver/controllers/CardController.java
@@ -1,15 +1,19 @@
 package com.wardencloud.wardenstashedserver.controllers;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wardencloud.wardenstashedserver.entities.StudyCard;
 import com.wardencloud.wardenstashedserver.services.StudyCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -20,8 +24,12 @@ public class CardController {
     private StudyCardService studyCardService;
 
     @GetMapping(value = "/studycard")
-    public Page<StudyCard> getAllStudyCards(Pageable pageable) {
+    public ResponseEntity getAllStudyCards(Pageable pageable) {
         int pageNumber = pageable.getPageNumber();
-        return studyCardService.findAllStudyCards(pageNumber);
+        Page<StudyCard> page = studyCardService.findAllStudyCards(pageNumber);
+        List<StudyCard> studyCards = page.getContent();
+        JSONObject jsonpObject = new JSONObject();
+        jsonpObject.put("data", studyCards);
+        return ResponseEntity.ok().body(jsonpObject);
     }
 }
