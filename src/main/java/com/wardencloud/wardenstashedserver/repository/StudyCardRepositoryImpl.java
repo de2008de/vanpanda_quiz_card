ec1220e74abc05a8c2dbbfcb781014b7bfc583ac
de2008de <mrycchou@gmail.com>
2019-09-19 23:41:59 -0700

    Implement POST addStudyCard controller api

--- src/main/java/com/wardencloud/wardenstashedserver/repository/StudyCardRepositoryImpl.java
+++ src/main/java/com/wardencloud/wardenstashedserver/repository/StudyCardRepositoryImpl.java
@@ -1,13 +1,14 @@
 package com.wardencloud.wardenstashedserver.repository;
 
+import com.wardencloud.wardenstashedserver.entities.ConceptCard;
 import com.wardencloud.wardenstashedserver.entities.StudyCard;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.Set;
 
 @Repository
 @Transactional
@@ -16,4 +17,20 @@ public class StudyCardRepositoryImpl implements StudyCardRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
+    public int addStudyCard(
+            String title,
+            String subtitle,
+            String school,
+            Set<ConceptCard> conceptCards
+    ) {
+        StudyCard studyCard = new StudyCard();
+        studyCard.setTitle(title);
+        studyCard.setSubtitle(subtitle);
+        studyCard.setSchool(school);
+        studyCard.setConceptCards(conceptCards);
+        entityManager.persist(studyCard);
+        entityManager.flush();
+        entityManager.refresh(studyCard);
+        return studyCard.getId();
+    }
 }
--- src/main/java/com/wardencloud/wardenstashedserver/services/StudyCardService.java
+++ src/main/java/com/wardencloud/wardenstashedserver/services/StudyCardService.java
@@ -1,8 +1,23 @@
 package com.wardencloud.wardenstashedserver.services;
 
+import com.wardencloud.wardenstashedserver.entities.ConceptCard;
+import com.wardencloud.wardenstashedserver.entities.KeyPoint;
 import com.wardencloud.wardenstashedserver.entities.StudyCard;
 import org.springframework.data.domain.Page;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public interface StudyCardService {
     Page<StudyCard> findAllStudyCards(int pageNumber);
+    int addStudyCard(
+            String title,
+            String subtitle,
+            String school,
+            Set<ConceptCard> conceptCards
+    );
+    Set<ConceptCard> convertListToConceptCardSet(List<Object> list);
+    ConceptCard convertMapToConceptCard(Map<Object, Object> map);
+    KeyPoint convertMapToKeyPoint(Map<Object, Object> map);
 }
